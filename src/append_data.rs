1939eae09f837091ed1633653d75d303389fffa6
gorkiy chocolate <beybamain@gmail.com>
2026-07-28 19:11:16 +0500

    file append

---
+++ src/append_data.rs
@@ -0,0 +1,14 @@
+use std::fs::OpenOptions;
+use std::io::Write;
+use std::path::PathBuf;
+
+pub fn append_data(structs: &str) -> std::io::Result<()> {
+    let mut docker_compose = OpenOptions::new()
+        .create(true) //create if doesnt exist
+        .append(true) //not overwrite file
+        .open("docker_compose.yml")?;
+
+    let dependency  = structs; //appending dependency into compose file
+    writeln!(file, user)?; 
+    Ok(())
+} 
